cac394afaca0cac7b93dffb6632577adbb78fe8c
hoangduylinh <hoangduylinh07@gmail.com>
2020-10-27 00:40:10 +0700

    a

---
+++ day11/java.js
@@ -0,0 +1,73 @@
+// day la chu thich 1 dong
+/* day la chu thich tren nhieu dong*/
+
+/* day la kieu du lieu chuoi, noi dung duoc dat trong dau ngoac don hoac kep - string*/
+
+//  
+
+// kieu du lieu so - number
+
+// var x = 5
+// var y = 5.5
+
+//kieu du lieu biilean(dung, sai)
+
+// var check = true
+// var uncheck= false
+
+// block scope
+
+// var newname = 'anna'
+// function getName(){
+//     newname='Linh'
+// }
+// console.log(newname)
+
+// var co hoisted (luon hien thi len dau)
+// let k co hoisted
+
+// viết hàm tính tổng 2 số:
+
+// function sumup (x,y) {
+// console.log(x+y)
+// alert(`tong cua 2 so la ${x} va ${y} la: ${x+y}`)
+// }
+// sumup (8,6)
+
+// function sendEmail (from, message = "khong co noi dung"){
+//     alert (from+","+ message)
+// sendEmail("robin")
+// };
+// function group (a,b,name) {
+//     alert(a+b+name)
+// }
+// group (1,22, john)
+
+// function group (a,b,c){
+//     alert(a+b+c)
+// }
+// group("$",4,5)
+
+// function group (a,b) {
+//     alert(a+b)
+// }
+// group(typeof(7),1);
+
+document.write('hello cac ban');
+document.write('<br/> xin chao');
+
+document.writeln('hu hu hu');
+
+// tính bình phương của 1 số
+function squareOfNumber(x){
+    Math.pow(x,2);
+}
+
+//tinh binh phuong cua tong (3a+2b-c)
+function binhphuong(a,b,c){
+   let tong=3*a + 2*b -c;
+   console.log ('ket qua cua phep toan 3*${a}+2*{b}-c la $math.pow(`3*${a}+2*{b}`-c,2)
+}
+
+
+
